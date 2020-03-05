4f72e40aabad2fe82705a87134da82da87749220
Andrea Seferi <dhimiter_14p@hotmail.com>
2020-03-05 15:26:18 +0100

    my file

---
+++ app.js
@@ -0,0 +1,20 @@
+const form = document.querySelector('#form');
+const content = document.querySelector('#content');
+
+const age = prompt('Hur gammal är du?');
+
+
+//Conditional Statement
+if(age > 20) {
+    //visa hemsidan
+    content.style.display = 'block';
+    const name = prompt('Vad heter du?');
+    document.querySelector('.message').innerHTML = "Välkommen " + name + "till Hemsidan";
+    document.querySelector('#enter').className = 'granted';
+} else {
+    //inte gammal nog
+    content.style.display = 'none';
+    document.querySelector('.message').innerHTML = 'Du är inte gammal nog,tyvärr.';
+    document.querySelector('#enter').className = 'denied';
+}
+
